59f0892df6138e03ddf74ccc8aaab219d884b3c7
YasinFaraji <yasinfaraji100@gmail.com>
2023-05-07 00:35:16 +0330

    Adding strcpy to string directory

--- src/kernel.c
+++ src/kernel.c
@@ -96,10 +96,9 @@ void kernel_main()
     // Enable the system interrupts
     enable_interrupts();
 
-    struct disk_stream* stream = diskstreamer_new(0);
-    diskstreamer_seek(stream, 0x201);
-    unsigned char c = 0;
-    diskstreamer_read(stream, &c, 1);
+    char buf[20];
+    strcpy(buf, "hello!");
+
     while (true)
     {
     }
