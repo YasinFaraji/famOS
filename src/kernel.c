8a0d5f92226dff8fc9bd767fac983d174c195009
YasinFaraji <yasinfaraji100@gmail.com>
2023-05-11 17:56:51 +0330

    Adding gdt directory

--- src/kernel.c
+++ src/kernel.c
@@ -5,11 +5,14 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "memory/memory.h"
 #include "string/string.h"
 #include "fs/file.h"
 #include "disk/disk.h"
 #include "fs/pparser.h"
 #include "disk/streamer.h"
+#include "gdt/gdt.h"
+#include "config.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -77,10 +80,23 @@ void panic(const char* msg)
     while (1) {}
 }
 
+struct gdt gdt_real[FAMOS_TOTAL_GDT_SEGMENTS];
+struct gdt_structured gdt_structured[FAMOS_TOTAL_GDT_SEGMENTS] = {
+    {.base = 0x00, .limit = 0x00, .type = 0x00},                     // NULL Segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x9a},               // Kernel code Segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x92}                // Kernel data Segment
+};
+
 void kernel_main()
 {
     terminal_initialize();
     print("Hello World!");
+
+    memset(gdt_real, 0x00, sizeof(gdt_real));
+    gdt_structured_to_gdt(gdt_real, gdt_structured, FAMOS_TOTAL_GDT_SEGMENTS);
+
+    // Load the gdt 
+    gdt_load(gdt_real, sizeof(gdt_real));
 
     // Initialize the heap 
     kheap_init();
