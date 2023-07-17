d2d1f919ef1bb2f5b058ab8b53a9ce4cfab8c9b5
YasinFaraji <yasinfaraji100@gmail.com>
2023-07-17 16:45:56 +0330

    Refactor some function in paging section

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -33,11 +33,11 @@ struct paging_4gb_chunk* paging_new_4gb(uint8_t flags)
     return chunk_4gb;
 }
 
-void paging_switch(uint32_t* directory)
+void paging_switch(struct paging_4gb_chunk* directory)
 {
-    paging_load_directory(directory);
+    paging_load_directory(directory->directory_entry);
 
-    current_directory = directory;
+    current_directory = directory->directory_entry;
 }
 
 void paging_free_4gb(struct paging_4gb_chunk* chunk)
@@ -89,17 +89,17 @@ void* paging_align_address(void* ptr)
     return ptr;
 }
 
-int paging_map(uint32_t* directory, void* virtual_address, void* physical_address, int flags)
+int paging_map(struct paging_4gb_chunk* directory, void* virtual_address, void* physical_address, int flags)
 {
     if (((unsigned int)virtual_address % PAGING_PAGE_SIZE) || ((unsigned int)physical_address % PAGING_PAGE_SIZE))
     {
         return -EINVARG;
     }
 
-    return paging_set(directory, virtual_address, (uint32_t)physical_address | flags);
+    return paging_set(directory->directory_entry, virtual_address, (uint32_t)physical_address | flags);
 }
 
-int paging_map_range(uint32_t* directory, void* virtual_address, void* physical_address, int count, int flags)
+int paging_map_range(struct paging_4gb_chunk* directory, void* virtual_address, void* physical_address, int count, int flags)
 {
     int res = 0;
 
@@ -118,7 +118,7 @@ int paging_map_range(uint32_t* directory, void* virtual_address, void* physical_
     return res;
 }
 
-int paging_map_to(uint32_t* directory, void* virtual_address, void* physical_address, void* physical_end_address, int flags)
+int paging_map_to(struct paging_4gb_chunk* directory, void* virtual_address, void* physical_address, void* physical_end_address, int flags)
 {
     int res = 0;
 
