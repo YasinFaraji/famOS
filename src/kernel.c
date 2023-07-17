17758c4688576c0b78b7671231959e0e699c7a09
YasinFaraji <yasinfaraji100@gmail.com>
2023-07-18 02:38:41 +0330

    Handling interrupt for classic keyboard

--- src/kernel.c
+++ src/kernel.c
@@ -103,10 +103,6 @@ struct gdt_structured gdt_structured[FAMOS_TOTAL_GDT_SEGMENTS] = {
     {.base = (uint32_t)&tss, .limit = sizeof(tss), .type = 0xE9}     // TSS Segment
 };
 
-void pic_timer_callback(struct interrupt_frame* frame)
-{
-    print("Timer activated\n");
-}
 
 void kernel_main()
 {
@@ -155,16 +151,12 @@ void kernel_main()
     // Initialize all the system keyboards 
     keyboard_init();
 
-    idt_register_interrupt_callback(0x20, pic_timer_callback);
-
     struct process* process = 0;
     int res = process_load_switch("0:/blank.bin", &process);
     if (res != FAMOS_ALL_OK)
     {
         panic("Failed to load blank.bin\n");
     }
-
-    keyboard_push('A');
 
     task_run_first_ever_task();
 
--- src/keyboard/classic.c
+++ src/keyboard/classic.c
@@ -37,7 +37,7 @@ void classic_keyboard_handle_interrupt();
 
 int classic_keyboard_init()
 {
-    // idt_register_interrupt_callback(ISR_KEYBOARD_INTERRUPT, classic_keyboard_handle_interrupt);
+    idt_register_interrupt_callback(ISR_KEYBOARD_INTERRUPT, classic_keyboard_handle_interrupt);
 
     // keyboard_set_capslock(&classic_keyboard, KEYBOARD_CAPS_LOCK_OFF);
 
@@ -67,18 +67,17 @@ uint8_t classic_keyboard_scancode_to_char(uint8_t scancode)
     return c;
 }
 
-
 void classic_keyboard_handle_interrupt()
 {
-    // kernel_page();
-    // uint8_t scancode = 0;
-    // scancode = insb(KEYBOARD_INPUT_PORT);
-    // insb(KEYBOARD_INPUT_PORT);
+    kernel_page();
+    uint8_t scancode = 0;
+    scancode = insb(KEYBOARD_INPUT_PORT);
+    insb(KEYBOARD_INPUT_PORT);
 
-    // if(scancode & CLASSIC_KEYBOARD_KEY_RELEASED)
-    // {
-    //     return;
-    // }
+    if(scancode & CLASSIC_KEYBOARD_KEY_RELEASED)
+    {
+        return;
+    }
 
     // if (scancode == CLASSIC_KEYBOARD_CAPSLOCK)
     // {
@@ -86,13 +85,13 @@ void classic_keyboard_handle_interrupt()
     //     keyboard_set_capslock(&classic_keyboard, old_state == KEYBOARD_CAPS_LOCK_ON ? KEYBOARD_CAPS_LOCK_OFF : KEYBOARD_CAPS_LOCK_ON);
     // }
 
-    // uint8_t c = classic_keyboard_scancode_to_char(scancode);
-    // if (c != 0)
-    // {
-    //     keyboard_push(c);
-    // }
+    uint8_t c = classic_keyboard_scancode_to_char(scancode);
+    if (c != 0)
+    {
+        keyboard_push(c);
+    }
 
-    // task_page();
+    task_page();
 }
 
 struct keyboard* classic_init()
