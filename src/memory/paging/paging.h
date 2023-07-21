39e5ecc040520cd6fdb43b2d7979c84e3486519a
YasinFaraji <yasinfaraji100@gmail.com>
2023-07-21 17:02:03 +0330

    Adding argument feature to isr80h

--- src/memory/paging/paging.h
+++ src/memory/paging/paging.h
@@ -38,6 +38,7 @@ int paging_map(struct paging_4gb_chunk* directory, void* virtual_address, void* 
 void* paging_align_address(void* ptr);
 uint32_t paging_get(uint32_t* directory, void* virt);
 void* paging_align_to_lower_page(void* addr);
+void* paging_get_physical_address(uint32_t* directory, void* virt);
 
 
 #endif
